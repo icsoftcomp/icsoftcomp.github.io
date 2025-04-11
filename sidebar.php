9a657cbcd5b50ffab58398fa45ae8419a82dcd41
icsoftcomp <icsoftcomp@gmail.com>
2025-04-11 14:33:36 +0530

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,109 @@
+<img src="images/update.gif"  width="100px" height="25px"><br>
+ 
+<font size="6px"><u>
+<a href="schedule1.pdf" target="_NEW"> Program - Schedule  </a> 
+<br><br>
+<img src="images/update.gif"  width="100px" height="25px"><br>
+<a href="acceptedpaperslist.pdf" target="_NEW"> List of Accepted papers </a> </font></u>
+<BR><BR></font></u>
+
+
+
+a href="https://www.scimagojr.com/journalsearch.php?q=17700155007&amp;tip=sid&amp;exact=no" title="SCImago Journal &amp; Country Rank" target="_NEW"><img border="0" src="https://www.scimagojr.com/journal_img.php?id=17700155007" alt="SCImago Journal &amp; Country Rank"  /></a>
+<br><BR>
+
+<a href="https://www.scopus.com/sourceid/17700155007" target="_NEW"><img src="images/ScopusR_Wmk_151_RGB.png" width="150" height="80"> </a>
+<a href="https://www.scopus.com/sourceid/17700155007?dgcid=sc_widget_citescore" style="text-decoration:none;color:#505050">
+<div style="height:100px;width:180px;font-family:Arial, Verdana, helvetica, sans-serif;background-color:#ffffff;display:inline-block">
+<div style="padding: 0px 16px;">
+<div style="padding-top:3px;line-height:1;">
+<div style="float:left;font-size:28px"><span id="citescoreVal" style="letter-spacing: -2px;display: inline-block;padding-top: 7px;line-height: .75;">1.0</span></div>
+<div style="float:right;font-size:14px;padding-top:3px;text-align: right;"><span id="citescoreYearVal" style="display:block;">2022</span>CiteScore</div>
+</div>
+<div style="clear:both;"></div>
+<div style="padding-top:3px;">
+<div style="height:4px;background-color:#DCDCDC;">
+<div style="height: 4px; background-color: rgb(0, 115, 152); width: 14%;" id="percentActBar">&nbsp;</div>
+</div>
+<div style="font-size:11px;"><span id="citescorePerVal">14th percentile</span></div>
+</div>
+<div style="font-size:12px;text-align:right;">Powered by &nbsp;<span><img alt="Scopus" style="width:50px;height:15px;" src="https://www.scopus.com/static/images/scopusLogoOrange.svg"></span></div>
+</div>
+</div>
+</a>
+
+		
+          <h3 class="into-sub-title">Important Dates</h3>
+		  
+		  <table>
+								<tr><TD>
+                              	Submission due: <TD>	<strike>30/06/2024 <br> 15/07/2024 <br> 31/07/2024 <br>15/08/2024 *Firm deadline</strike><br>Closed
+								<tr><TD>
+								<br>Acceptance Notification:	<td><br><strike>31/07/2024<br> 31/08/2024 </strike><br>09/09/2024 
+								<tr><TD>
+								<br>Camera Ready Paper Submission due:	<td>According to notification
+								<tr><TD>
+								<br>Last date of registration:	<td><br>According to notification
+								<tr><TD>
+								<br>Conference dates:	<td>10-12/12/2024
+								</table>
+          
+		  <h3 class="into-sub-title">Awards</h3>
+		  Two best paper awards<bR>
+- Best Research Paper Award, and<br>
+- Best Student Research Paper Award<br>
+will be announced and bestowed at the conference valedictory session. 
+			<BR>
+			<h3 class="into-sub-title">Call For Reviewers</h3>
+			<u><a href="https://forms.gle/13PBRjmMCscUJM8g7" target="_NEW">Registration link </a></u>
+			<br>Reviewers will get recognition on conference website and a certificate of appreciation.
+		  <!--kkp
+		  <p>Minim Austin 3 wolf moon scenester aesthetic, umami odio pariatur bitters. Pop-up occaecat taxidermy street art, tattooed beard literally.</p>
+
+          <div class="accordion accordion-group" id="our-values-accordion">
+              <div class="card">
+                <div class="card-header p-0 bg-transparent" id="headingOne">
+                    <h2 class="mb-0">
+                      <button class="btn btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                          Safety
+                      </button>
+                    </h2>
+                </div>
+              
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#our-values-accordion">
+                    <div class="card-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidata
+                    </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header p-0 bg-transparent" id="headingTwo">
+                    <h2 class="mb-0">
+                      <button class="btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                          Customer Service
+                      </button>
+                    </h2>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#our-values-accordion">
+                    <div class="card-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidata
+                    </div>
+                </div>
+              </div>
+              <div class="card">
+                <div class="card-header p-0 bg-transparent" id="headingThree">
+                    <h2 class="mb-0">
+                      <button class="btn btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                          Integrity
+                      </button>
+                    </h2>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#our-values-accordion">
+                    <div class="card-body">
+                      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidata
+                    </div>
+                </div>
+              </div>
+          </div>
+		  kkp -->
+          <!--/ Accordion end -->
